9bda5614c8e3b1ff4ee81e23ed423f032d0873a6
SARATH BABU <srth12@gmail.com>
2018-04-07 00:14:18 +0530

    Class in python

--- Coding Practice/codingpractice/src/main/python/classes_start.py
+++ Coding Practice/codingpractice/src/main/python/classes_start.py
@@ -2,8 +2,28 @@
 # Example file for working with classes
 #
 
+class MyClass:
+  def method1(self):
+      print("This is method 1")
 
+  def method2(self, arg):
+      print("This is method 2 " + arg)
+
+
+class AnotherClass(MyClass):
+    def method1(self):
+        MyClass.method1(self)
+        print("This is Another class method 1")
+
+    def method2(self, arg):
+        print("This is another class method 2 " + arg)
 def main():
+    #c = MyClass()
+    #c.method1();
+    #c.method2(" and argument is me")
+    d = AnotherClass()
+    d.method1()
+    d.method2(" and arg me")
 
   
 if __name__ == "__main__":
