055684b36d9ab5e67a654f25b970965b0fa4106f
SARATH BABU <srth12@gmail.com>
2018-05-19 16:26:57 +0530

    Sum at level k of a tree

--- Coding Practice/codingpractice/src/main/java/com/hackerrank/code/graph/SumOfElementsAtLevelK.java
+++ Coding Practice/codingpractice/src/main/java/com/hackerrank/code/graph/SumOfElementsAtLevelK.java
@@ -2,5 +2,26 @@ package com.hackerrank.code.graph;
 
 public class SumOfElementsAtLevelK {
 
+    public static void main(String[] args) {
+        String input = "(0(5(6()())(4()(9()())))(7(1()())(3()())))";
+        int result = getSumOfLevel(input, 2);
+        System.out.println(result);
+    }
+
+    public static int getSumOfLevel(String input, int k){
+        int sum = 0;
+        int level = -1;
+        for (char c: input.toCharArray()){
+            if (c == '(') level++;
+            else if (c == ')') level--;
+            else {
+                if (level == k){
+                    sum += Character.getNumericValue(c);
+                }
+            }
+        }
+
+        return sum;
+    }
 
 }
