d7f785f2ab4f6746f143740a5c6fe06c50d26027
SARATH BABU <srth12@gmail.com>
2020-03-29 11:04:33 +0530

    paypay test

--- Coding Practice/codingpractice/src/main/java/com/hackerrank/code/AtomicInt.java
+++ Coding Practice/codingpractice/src/main/java/com/hackerrank/code/AtomicInt.java
@@ -1,4 +1,19 @@
 package com.hackerrank.code;
 
-public class AtomicInteger {
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicInt {
+    final public AtomicInteger ai = new AtomicInteger();
+    public void atomset(){
+        ai.set(123);
+    }
+    public int getNumber(){
+        return ai.get();
+    }
+
+    public static void main(String[] args) {
+        AtomicInt a = new AtomicInt();
+        a.atomset();
+        System.out.println(a.getNumber());
+    }
 }
--- Coding Practice/codingpractice/src/main/python/interview/paypay/paypay.py
+++ Coding Practice/codingpractice/src/main/python/interview/paypay/paypay.py
@@ -1,23 +1,23 @@
-Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
-
-Your algorithm's runtime complexity must be in the order of O(log n).
-
-If the target is not found in the array, return [-1, -1].
-
-昇順にソートされた整数の配列があるとして、その中から特定の値の最初と最後の位置を配列として返すプログラムを作成してください
-
-計算量はO(log n)である必要があります
-
-対象が見つからない場合の結果は[-1, -1]としてください
-
-Example 1:
-
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-Example 2:
-
-Input: nums = [5,7,7,8,8,10], target = 6
-Output: [-1,-1]
+# Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+#
+# Your algorithm's runtime complexity must be in the order of O(log n).
+#
+# If the target is not found in the array, return [-1, -1].
+#
+# 昇順にソートされた整数の配列があるとして、その中から特定の値の最初と最後の位置を配列として返すプログラムを作成してください
+#
+# 計算量はO(log n)である必要があります
+#
+# 対象が見つからない場合の結果は[-1, -1]としてください
+#
+# Example 1:
+#
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+#
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
 
 class Test:
 
